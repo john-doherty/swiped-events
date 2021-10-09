c360442b6a9daee0ffb0006c3fb775ce3612fa7f
John Doherty <contact@johndoherty.info>
2021-10-09 14:00:19 +0100

    set default `e.detail.touchType` to `direct`

--- src/swiped-events.js
+++ src/swiped-events.js
@@ -75,7 +75,7 @@
 
             var eventData = {
                 dir: eventType.replace(/swiped-/, ''),
-                touchType: (changedTouches[0] || {}).touchType,
+                touchType: (changedTouches[0] || {}).touchType || 'direct',
                 xStart: parseInt(xDown, 10),
                 xEnd: parseInt((changedTouches[0] || {}).clientX || -1, 10),
                 yStart: parseInt(yDown, 10),
